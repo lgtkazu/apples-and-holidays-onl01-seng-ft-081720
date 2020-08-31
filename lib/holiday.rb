e2b4b508542bb440d839042301e544c9f12c3b61
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-31 01:29:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,10 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-@@ -53,15 +54,50 @@ def all_supplies_in_holidays(holiday_hash)
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
@@ -99,7 +102,6 @@ end
 answer
 
 end
-
 
 
 
